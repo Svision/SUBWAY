52836f02c561f51d156368a50fd724656b0fca78
Changhao Song <changhao.song@mail.utoronto.ca>
2022-09-17 15:05:43 -0400

    Initial Commit

---
+++ Subway Counter/Subway_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Subway_CounterApp.swift
+//  Subway Counter
+//
+//  Created by Changhao Song on 2022-09-17.
+//
+
+import SwiftUI
+
+@main
+struct Subway_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
